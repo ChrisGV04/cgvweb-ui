c25c5b822fa997c7f8fb0175dc3fd5eb8a42b294
Christian Gil <mrcgam.christian@gmail.com>
2024-03-12 20:34:24 -0600

    refactor(Container): Convert to Primitive

--- src/runtime/components/layout/Container.vue
+++ src/runtime/components/layout/Container.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 // @ts-expect-error
 import appConfig from '#build/app.config';
+
 import { useUI } from '#ui/composables/useUI';
 import type { Strategy } from '#ui/types';
 import { container } from '#ui/ui.config';
 import { mergeConfig } from '#ui/utils';
+import { Primitive, type PrimitiveProps } from 'radix-vue';
 import { twJoin, twMerge } from 'tailwind-merge';
-import type { PropType } from 'vue';
-import { defineOptions, toRef } from 'vue';
+import { toRef, withDefaults } from 'vue';
 
 const config = mergeConfig<typeof container>(
   appConfig.ui.container.strategy,
@@ -16,29 +17,20 @@ const config = mergeConfig<typeof container>(
 );
 type UiConfig = Partial<typeof config> & { strategy?: Strategy };
 
-defineOptions({ inheritAttrs: false });
-
-const props = defineProps({
-  as: { type: String, default: 'div' },
-  class: {
-    type: [String, Object, Array] as PropType<any>,
-    default: undefined,
-  },
-  ui: {
-    type: Object as PropType<UiConfig>,
-    default: () => ({}) as UiConfig,
-  },
+const props = withDefaults(defineProps<PrimitiveProps & { class?: any; ui?: UiConfig }>(), {
+  as: 'div',
+  ui: () => ({}) as UiConfig,
 });
 
-const { ui, attrs } = useUI('container', toRef(props, 'ui'), config);
+const { ui } = useUI('container', toRef(props, 'ui'), config);
 </script>
 
 <template>
-  <component
-    :is="as"
-    :class="twMerge(twJoin(ui.base, ui.xPadding, ui.yPadding, ui.constrained), props.class)"
-    v-bind="attrs"
+  <Primitive
+    :as="props.as"
+    :as-child="props.asChild"
+    :class="twMerge(twJoin(ui.base, ui.constrained, ui.xPadding, ui.yPadding), props.class)"
   >
     <slot />
-  </component>
+  </Primitive>
 </template>
